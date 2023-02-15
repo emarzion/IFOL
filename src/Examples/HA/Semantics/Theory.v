89d1398a943fd46091a83736b374f9b7589ef18e
Evan Marzion <emarzion@gmail.com>
2023-02-14 23:44:41 -0600

    HA example (#2)
    * adding axioms
    
    * rm checks and stuff
    
    * adding HA example

---
+++ src/Examples/HA/Semantics/Theory.v
@@ -0,0 +1,29 @@
+Require Import Lia.
+
+Require Import IFOL.Semantics.Signature.
+Require Import IFOL.Semantics.Theory.
+
+Require Import IFOL.Examples.HA.Syntax.Signature.
+Require Import IFOL.Examples.HA.Syntax.Theory.
+Require Import IFOL.Examples.HA.Semantics.Signature.
+Require Import IFOL.Examples.HA.Semantics.Sort.
+
+Definition eval_HA_theory :
+  theory_dom eval_HA_sort HA_sig eval_HA_sig HA_theory.
+Proof.
+  intro ax.
+  destruct ax.
+  - exact PeanoNat.Nat.neq_0_succ.
+  - exact eq_add_S.
+  - exact plus_O_n.
+  - exact PeanoNat.Nat.add_succ_l.
+  - exact PeanoNat.Nat.mul_0_l.
+  - exact PeanoNat.Nat.mul_succ_l.
+  - simpl.
+    rewrite Formula.eval_Formula_subst.
+    simpl.
+    intros.
+    induction x.
+    + exact H.
+    + admit.
+Admitted.
---
+++ src/Examples/HA/Syntax/Sort.v
@@ -0,0 +1,3 @@
+
+Inductive HA_sort :=
+  | Nat.
